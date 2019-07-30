33366a2de8280d464125506e61d3cc8e53b62757
Győri Sándor <alexaegis@gmail.com>
2019-07-30 19:13:15 +0200

    tsconf

---
+++ apps/demo-app/src/app/store/reducers/index.ts
@@ -0,0 +1,8 @@
+import { environment } from '@env';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+
+export interface State {}
+
+export const reducers: ActionReducerMap<State> = {};
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
