d24faac8bfb84251e37302b8126ccb862694caf3
Győri Sándor <alexaegis@gmail.com>
2019-08-18 12:56:11 +0200

    cors enabled

--- apps/demo-app/src/app/core/modules/auth/services/auth-api.service.ts
+++ apps/demo-app/src/app/core/modules/auth/services/auth-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginRequest } from '@app/store/actions';
+import { environment } from '@env';
 import { of } from 'rxjs';
 
 @Injectable({
@@ -10,6 +11,7 @@ export class AuthApiService {
 	public constructor(private http: HttpClient) {}
 
 	public login(request: LoginRequest) {
+		return this.http.get(`${environment.api}`);
 		console.log(request);
 		return of({ token: 'mock' });
 	}
--- apps/demo-server/src/main.ts
+++ apps/demo-server/src/main.ts
@@ -9,6 +9,9 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const globalPrefix = 'api';
+	app.enableCors({
+		origin: /https?:\/\/localhost.*/
+	});
 	app.setGlobalPrefix(globalPrefix);
 	const port = process.env.port || 3333;
 	await app.listen(port, () => {
