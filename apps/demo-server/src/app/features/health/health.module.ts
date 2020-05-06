18ca6deda4012dfa1be26a5e0941375c424e181e
AlexAegis <alexaegis@gmail.com>
2020-05-06 23:56:25 +0200

    health

--- apps/demo-server/src/app/features/health/health.module.ts
+++ apps/demo-server/src/app/features/health/health.module.ts
@@ -1,31 +1,12 @@
 import { DatabaseModule } from '@app/database/database.module';
 import { Module } from '@nestjs/common';
-import { DiskHealthIndicator, TerminusModule, TerminusModuleOptions } from '@nestjs/terminus';
-
-const getTerminusOptions = (disk: DiskHealthIndicator): TerminusModuleOptions => ({
-	endpoints: [
-		{
-			// The health check will be available with /health
-			url: '/health',
-			// All the indicator which will be checked when requesting /health
-			healthIndicators: [
-				async () => disk.checkStorage('disk', { path: __dirname, thresholdPercent: 0.9 }),
-			],
-		},
-	],
-});
+import { TerminusModule } from '@nestjs/terminus';
 
 /**
  * Exposes a healthcheck endpoint which internally checkes services, in this case, the database
  * that it's alive and well
  */
 @Module({
-	imports: [
-		DatabaseModule,
-		TerminusModule.forRootAsync({
-			inject: [DiskHealthIndicator],
-			useFactory: (disk) => getTerminusOptions(disk),
-		}),
-	],
+	imports: [DatabaseModule, TerminusModule],
 })
 export class HealthModule {}
